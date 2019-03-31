f6c4f4f9cd39a958c4c29b59cbc107045312b331
rnjsdnfka <rnjsdnfka123@naver.com>
2019-03-31 20:32:03 +0900

    Firebase Database

--- app/src/main/java/com/example/hobbigation/MainActivity.java
+++ app/src/main/java/com/example/hobbigation/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.hobbigation;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Write a message to the database
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
 
 
     //hi hi im jh
@@ -11,5 +18,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myRef.setValue("Hello, World!");
     }
 }
