578d4e0307cf72fdf66788346f6708704e8836a0
rnjsdnfka <rnjsdnfka123@naver.com>
2019-04-01 10:42:50 +0900

    Status Bar Color -> Black

--- app/src/main/java/com/example/hobbigation/LoginActivity.java
+++ app/src/main/java/com/example/hobbigation/LoginActivity.java
@@ -1,13 +1,25 @@
 package com.example.hobbigation;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
+
+    private Button back_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        back_btn = (Button) findViewById(R.id.button2);
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
