801c0f4bd0054721027971a3a51c2e90ca03b75d
rodash3 <seoroda@gmail.com>
2019-04-30 20:11:13 +0900

    Push Messaging

--- app/src/main/java/com/example/hobbigation/MainActivity.java
+++ app/src/main/java/com/example/hobbigation/MainActivity.java
@@ -2,17 +2,21 @@ package com.example.hobbigation;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.iid.FirebaseInstanceId;
+
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth firebaseAuth;
-
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     private TextView show_status;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,11 +28,16 @@ public class MainActivity extends AppCompatActivity {
         firebaseAuth = FirebaseAuth.getInstance();
         FirebaseUser mFirebaseUser = firebaseAuth.getCurrentUser();
 
+        FirebaseInstanceId.getInstance().getToken();
 
-        if (mFirebaseUser != null) {
-            show_status.setText(mFirebaseUser.getEmail()+"반갑습니다");
+        if (FirebaseInstanceId.getInstance().getToken() != null) {
+            Log.d(TAG, "token = " + FirebaseInstanceId.getInstance().getToken());
+
+            if (mFirebaseUser != null) {
+                show_status.setText(mFirebaseUser.getEmail() + "반갑습니다");
+            }
+
         }
-
     }
 }
 
