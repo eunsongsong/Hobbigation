415df4a01195312b35c12e2e08e7612a844cfb06
rodash3 <seoroda@gmail.com>
2019-05-14 13:03:07 +0900

    typing mistake correction

--- app/src/main/java/com/example/hobbigation/TabFragment2.java
+++ app/src/main/java/com/example/hobbigation/TabFragment2.java
@@ -5,15 +5,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
-import android.widget.ImageView;
-import com.bumptech.glide.Glide;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -137,9 +133,8 @@ public class TabFragment2 extends Fragment implements View.OnClickListener {
                 categorySelect("휴식");
                 break;
             case R.id.back_img_vol12:
-                categorySelect("봉사활등");
+                categorySelect("봉사활동");
                 break;
-
         }
 
     }
