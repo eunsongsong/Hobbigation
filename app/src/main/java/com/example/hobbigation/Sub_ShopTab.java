2e0e8d6d0ae8812ce75c51bf715d7832a84fb7ae
rodash3 <seoroda@gmail.com>
2019-06-03 15:09:48 +0900

    Modify Cafe Tab - Cafe post link + Cafe home link

--- app/src/main/java/com/example/hobbigation/Sub_ShopTab.java
+++ app/src/main/java/com/example/hobbigation/Sub_ShopTab.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -35,7 +34,7 @@ public class Sub_ShopTab extends Fragment {
             public void run() {
                 try {
                     strshop = getNaverShoppingSearch(keyword);
-                    Log.d("널이냐?", strshop);
+
                     getActivity().runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
